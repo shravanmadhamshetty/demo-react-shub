2a6749012ae4350736dc9a624b63e23b294c1f3e
shravanmadhamshetty <shravanmadhamshetty1995@gmail.com>
2022-11-01 22:48:42 +0530

    React bootstrap table initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import * as ReactBootStrap from "react-bootstrap";
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 200,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 300,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 400,
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 500,
+    },
+    {
+      id: 5,
+      name: "Product 1",
+      price: 200,
+    },
+    {
+      id: 6,
+      name: "Product 2",
+      price: 300,
+    },
+    {
+      id: 7,
+      name: "Product 3",
+      price: 400,
+    },
+    {
+      id: 8,
+      name: "Product 4",
+      price: 500,
+    },
+    {
+      id: 9,
+      name: "Product 1",
+      price: 200,
+    },
+    {
+      id: 10,
+      name: "Product 2",
+      price: 300,
+    },
+    {
+      id: 11,
+      name: "Product 3",
+      price: 400,
+    },
+    {
+      id: 12,
+      name: "Product 4",
+      price: 500,
+    },
+  ];
+  const columns = [
+    {
+      dataField: "id",
+      text: "Product ID",
+      sort: true,
+    },
+    {
+      dataField: "name",
+      text: "Product Name",
+    },
+    {
+      dataField: "price",
+      text: "Product Price",
+    },
+  ];
+
+  const defaultSorted = [
+    {
+      dataField: "id",
+      order: "desc",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BootstrapTable
+        bootstrap4
+        keyField="id"
+        data={products}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        pagination={paginationFactory()}
+      />
     </div>
   );
 }
